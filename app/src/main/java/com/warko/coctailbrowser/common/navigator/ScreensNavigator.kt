93e0932c5c1baf6077b399cfbe7b85c6ed17f95e
Warko <swartalf222@gmail.com>
2023-02-07 13:56:14 +0200

    Raw mvi is ready

---
+++ app/src/main/java/com/warko/coctailbrowser/common/navigator/ScreensNavigator.kt
@@ -0,0 +1,9 @@
+package com.warko.coctailbrowser.common.navigator
+
+interface ScreensNavigator {
+
+    fun navigateTo(screen: Screen)
+
+    fun back()
+
+}
---
+++ app/src/main/java/com/warko/coctailbrowser/feature/ingredientsearch/IngredientSearchActivity.kt
@@ -0,0 +1,19 @@
+package com.warko.coctailbrowser.feature.ingredientsearch
+
+import androidx.compose.runtime.Composable
+import com.warko.coctailbrowser.common.BaseActivity
+
+class IngredientSearchActivity : BaseActivity<IngredientSearchViewModel>() {
+
+    override val viewModel: IngredientSearchViewModel
+        get() = TODO("Not yet implemented")
+
+    override fun injectDependencies() {
+        TODO("Not yet implemented")
+    }
+
+    @Composable
+    override fun ScreenContent() {
+        TODO("Not yet implemented")
+    }
+}
