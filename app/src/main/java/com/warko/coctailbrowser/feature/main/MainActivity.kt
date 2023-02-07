2bd5b7f2b3749af3b96befd04b123c21f68a147a
Warko <swartalf222@gmail.com>
2023-02-07 12:26:51 +0200

    Finished with mvvm setup

--- app/src/main/java/com/warko/coctailbrowser/feature/main/MainActivity.kt
+++ app/src/main/java/com/warko/coctailbrowser/feature/main/MainActivity.kt
@@ -1,56 +1,20 @@
 package com.warko.coctailbrowser.feature.main
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.warko.coctailbrowser.common.BaseActivity
-import com.warko.coctailbrowser.ui.theme.CoctailBrowserTheme
 
 class MainActivity : BaseActivity<MainActivityViewModel>() {
 
     override val viewModel: MainActivityViewModel by viewModel()
 
     override fun injectDependencies() {
-        TODO("Not yet implemented")
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            CoctailBrowserTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        DaggerMainActivityComponent.factory()
+            .create(this)
+            .inject(this)
     }
 
     @Composable
     override fun ScreenContent() {
-        TODO("Not yet implemented")
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CoctailBrowserTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
---
+++ app/src/main/java/com/warko/coctailbrowser/feature/main/MainActivityUi.kt
@@ -0,0 +1,62 @@
+package com.warko.coctailbrowser.feature.main
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.warko.coctailbrowser.ui.theme.CoctailBrowserTheme
+
+@Composable
+fun MainScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar() {
+                Text(text = "Cocktail browser", modifier = Modifier.padding(start = 16.dp))
+            }
+        }) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize(), verticalArrangement = Arrangement.Center
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(text = "Search for a cocktail")
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(text = "Search for an ingredient")
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(text = "Show random cocktail")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    CoctailBrowserTheme {
+        MainScreen()
+    }
+}
